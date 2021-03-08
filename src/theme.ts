745295944f23859ba6ae9c034c93fa650887611f
dominik-zeglen <flesz3@o2.pl>
2021-03-08 15:42:01 +0100

    Fix disabled text button color

--- src/theme.ts
+++ src/theme.ts
@@ -244,7 +244,7 @@ const createTheme = (colors: IThemeColors): ITheme =>
           }
         },
         textPrimary: {
-          "& span": {
+          "&:not($disabled) span": {
             color: colors.primary
           }
         }
